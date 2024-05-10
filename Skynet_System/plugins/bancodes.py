b7566c4876e1b59b614184bff217d5fa5fa0d909
『itzzzyashu™』 <83384892+itzzzyashu@users.noreply.github.com>
2024-05-10 18:57:57 +0530

    Update bancodes.py

--- Skynet_System/plugins/bancodes.py
+++ Skynet_System/plugins/bancodes.py
@@ -1,26 +1,28 @@
    
-help_plus = """ Here is Help for **Bancodes** :-
-**❂** `{SXSx00}` **SCAMMER.**
-**❂** `{SXSx01}` **SPAM ADDING MEMBER.**
-**❂** `{SXSx03}` **ABUSE SPAM.**
-**❂** `{SXSx04}` **NSFW SPAMMER.**
-**❂** `{SXSx06}` **IMPERSONATION.**
-**❂** `{SXSx07}` **MD/BTC SCAM.**
-**❂** `{SXSx08}` **ADDING SPAMBOTS.**
-**❂** `{SXSx10}` **ILLEGAL.**
-**❂** `{SXSx11}` **PHISHING.**
-**❂** `{SXSx12}` **FRAUD PROMOTION  [ANY KIND].**
-**❂** `{SXSx13}` **CYBER THREATENING / CYBER BULLY.**
-**❂** `{SXSx14}` **CHILD ABUSE.**
-**❂** `{SXSx15}` **BAN EVASION.**
-**❂** `{SXSx16}` **SPAMBOT.**
-**❂** `{SXSx17}` **RAID INITIALIZOR.** 
-**❂** `{SXSx18}` **RAID PARTICIPANT.**
-**❂** `{SXSx19}` **KRIMINALANT.**
-**❂** `{SXSx20}` **SPAMMING VIOLENT CONTENT.**
-**❂** `{SXSx21}` **HATE SPEECH AGAINST NATIONALITY.**
-**❂** `{SXSx22}` **RAID/SPAM INFLAMMER.**
-**❂** `{SXSx23}` **PRONOGRAPHY CCONTENT PROMOTING.**
-**❂** `{SXSx24}` **PAID GIRL.**
+help_plus = """ Here is Help for **Bancodes**
+
+**◈** `{UXx01}` - **Involving in cyber scam activities.**
+**◈** `{UXx02}` - **Scraping members or adding users in a group/channel without their consent.**
+**◈** `{UXx03}` - **Usage of vulgar or inappropriate languages such as abuses in a group or in private.**
+**◈** `{UXx04}` - **NSFW content spam in inbox or in a group.**
+**◈** `{UXx05}` - **Impersonation of any user or copying someone's identity including name, profile pics.**
+**◈** `{UXx06}` - **MD/BTC or online scam activities.**
+**◈** `{UXx07}` - **Adding spambots in a group.**
+**◈** `{UXx08}` - **Illegal activities on cyber platforms or internet.**
+**◈** `{UXx09}` - **Attempting of phishing, hacking, or trying to steal someone's privacy.**
+**◈** `{UXx10}` - **Any kind of fraud or scam related promotion.**
+**◈** `{UXx11}` - **Cyber threatening or bullying including hateful abuses, incest or blackmailing.**
+**◈** `{UXx12}` - **Attempt of child abuse (physical, sexual, emotional or psychological)/sharing content related to child pornography or any kind of rape.**
+**◈** `{UXx13}` - **Any kind of ban evasion, threatening or misuse of ownership or adminship of a bot, group or a channel.**
+**◈** `{UXx14}` - **Any bot which is made to spam inbox, group chats or channel.**
+**◈** `{UXx15}` - **Initializer of illegal spam raid activities including raising people to do such.**
+**◈** `{UXx16}` - **Participating in illegal activities such as spam raid.**
+**◈** `{UXx17}` - **Criminal activity according to cyber laws in general and against telegram policies.**
+**◈** `{UXx18}` - **Spam activity of violent or sensitive content such as gore.**
+**◈** `{UXx19}` - **Hate speech against any nationality, culture, religion, caste or racial discrimination.**
+**◈** `{UXx20}` - **Raid Spamming/Influencer.**
+**◈** `{UXx21}` - **Selling or promotion of illegal pornographic content.**
+**◈** `{UXx22}` - **Paid sexual activities, Nude or selling sexually sensitive content.**
+
 """
 __plugin_name__ = "bancodes"
